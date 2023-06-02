dfb09f5ea02f1eea30e293f76d29858fbdcc50c3
Emilkroldrup <113909369+Emilkroldrup@users.noreply.github.com>
2023-06-02 23:24:15 +0200

    :))

--- vite-react-app/src/Gadgets/StyledComponents.tsx
+++ vite-react-app/src/Gadgets/StyledComponents.tsx
@@ -1,14 +1,16 @@
 import { styled } from "@mui/system";
 
 export const StyledCalendarBox = styled('div')({
-    extend: 'calendar-box',
     border: '1px solid #ccc',
     padding: '8px',
+    display: 'inline-block',
+    backgroundColor: 'Grey',
+    
     
 });
 
 export const StyledCalendarDay = styled('div')({
-    extend: 'calendar-day',
     border: '1px solid #ccc',
     padding: '8px',
+    backgroundColor: 'White',
 });
