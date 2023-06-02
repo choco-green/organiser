52f920a2778f39f7401f54f4fce523aa0c65d4ae
Emilkroldrup <113909369+Emilkroldrup@users.noreply.github.com>
2023-06-02 23:14:29 +0200

    I lost my brain cells

--- vite-react-app/src/Gadgets/StyledComponents.tsx
+++ vite-react-app/src/Gadgets/StyledComponents.tsx
@@ -2,8 +2,13 @@ import { styled } from "@mui/system";
 
 export const StyledCalendarBox = styled('div')({
     extend: 'calendar-box',
+    border: '1px solid #ccc',
+    padding: '8px',
+    
 });
 
 export const StyledCalendarDay = styled('div')({
     extend: 'calendar-day',
+    border: '1px solid #ccc',
+    padding: '8px',
 });
