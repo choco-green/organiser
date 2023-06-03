2adb688a88a2966c4a017e402c60e1fe78b39316
Emilkroldrup <113909369+Emilkroldrup@users.noreply.github.com>
2023-06-03 20:02:31 +0200

    nej

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import CalendarBox from './Gadgets/Calendar/Calendar'
 import Notes from './Gadgets/Notes/Notes'
 import './App.css'
+import Calendar from './Gadgets/Calendar/Calendar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,7 @@ function App() {
       <h1>Hello you Sexy beast</h1>
       <div className="gadget-container">
         <Notes />
-        <CalendarBox />
+        <Calendar />
       </div>
     </>
   )
--- client/src/Gadgets/Calendar/Calendar.tsx
+++ client/src/Gadgets/Calendar/Calendar.tsx
@@ -1,40 +1,16 @@
-import React from "react";
-import { Typography } from "@mui/material";
-import { StyledCalendarBox, StyledCalendarDay } from "./StyledComponents";
-import "./Calendar.css";
+import * as React from 'react';
+import dayjs, { Dayjs } from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 
-//interface CalendarBoxProps {
-//}
+const Calendar = () => {
+  const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-17'));
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+       <DateCalendar value={value} onChange={(newValue) => setValue(newValue)} />
+    </LocalizationProvider>
+  );
+}
 
-
-const CalendarBox: React.FC<CalendarBoxProps> = () => {
-
-    // some fake things to make it look pretty until choco gets going ;) 
-    const calendarData = [
-        { day: 1, event: 'Meeting with the boss' },
-        { day: 2, event: 'Birthday big Choco' },
-        { day: 3, event: 'Vacation bitches' },
-    ];
-
-    return (
-        <StyledCalendarBox>
-          <Typography variant="h5" gutterBottom>
-            Calendar
-          </Typography>
-          <div className="calendar">
-            {calendarData.map(({ day, event }) => (
-              <StyledCalendarDay key={day}>
-                <Typography variant="h6" fontWeight="bold">
-                  {day}
-                </Typography>
-                <Typography variant="subtitle1" color="text.secondary">
-                  {event}
-                </Typography>
-              </StyledCalendarDay>
-            ))}
-          </div>
-        </StyledCalendarBox>
-      );
-    };
-
-export default CalendarBox;
+export default Calendar;
