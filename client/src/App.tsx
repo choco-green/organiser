fefc12485fc847c27b10b6631a48c4c3410ba3a5
choco_green <choco_green@hotmail.com>
2023-06-03 12:11:34 +0100

    Revert "Merge (2/2)"
    This reverts commit f6eaf65e5821586a56abb8f44db4a615a8841a2a.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,17 @@
-import CalendarBox from './Gadgets/Calendar/Calendar'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import CalendarBox from './Gadgets/Calendar'
 import './App.css'
-import Notes from './Gadgets/Notes/Notes'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Hello you Sexy beast</h1>
-      <div className="gadget-contrainer">
+      <div>
         <CalendarBox />
-        <Notes />
       </div>
     </>
   )
